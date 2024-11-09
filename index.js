7d68da57f228300646634a80c8c7222c7b178de1
Yash Rupavatiya <yashrupavatiya@Yash-2.local>
2024-11-09 20:37:19 +0530

    buildspec file 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const dynamoDB = require('./dynamodb'); // Use the new DynamoDB file
 
-const port = 3000;
+const port = 80;
 const app = express();
 
 app.set('view engine', 'ejs');
