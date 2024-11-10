8fea5eb36064a622ff14e608ec52f5399d23659d
Yash Rupavatiya <yashrupavatiya@Yash-2.local>
2024-11-10 20:05:52 +0530

    appspec updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 sudo apt update -y
-sudo apt install -y unzip nodejs npm awscli
+sudo apt install unzip -y nodejs npm awscli
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt install awscli -y
 aws s3 cp s3://codebuild-cicd-monitoring/build.zip /home/ubuntu/build.zip
 unzip -o /home/ubuntu/build.zip -d /home/ubuntu/build
 cd /home/ubuntu/build
