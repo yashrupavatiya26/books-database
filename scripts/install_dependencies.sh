290db12532d35582a04ae3dfe3d6deaf96cf1507
Yash Rupavatiya <yashrupavatiya@Yash-2.local>
2024-11-12 01:51:09 +0530

    install_dependencies updated 3

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-set -e 
+set -e
+
 echo "Starting installation of dependencies..."
+
 echo "Updating package list..."
 sudo apt update -y
+
 echo "Installing necessary dependencies..."
 sudo apt install -y unzip nodejs npm curl
+
 echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install
+
 echo "Verifying AWS CLI installation..."
 aws --version
-cd /opt/codedeploy-agent/deployment-root/*/deployment-archive/
-if [ ! -f "build.zip" ]; then
-  echo "Error: build.zip not found in the deployment directory"
-  exit 1
-fi
+
 echo "Downloading build artifact from S3..."
 S3_BUCKET="codebuild-cicd-monitoring"
 S3_OBJECT="build.zip"
-aws s3 cp s3://$S3_BUCKET/$S3_OBJECT /opt/codedeploy-agent/deployment-root/*/deployment-archive/build.zip
+aws s3 cp s3://$S3_BUCKET/$S3_OBJECT /home/ubuntu/build.zip
+
 echo "Extracting build artifact..."
-unzip -o /opt/codedeploy-agent/deployment-root/*/deployment-archive/build.zip -d /home/ubuntu/build
+unzip -o /home/ubuntu/build.zip -d /home/ubuntu/build
+
 echo "Installing Node.js dependencies..."
 cd /home/ubuntu/build
 npm install
